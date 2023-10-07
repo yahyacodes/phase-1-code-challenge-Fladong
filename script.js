c96475808bc5ebedf365fd06b6fb053d476cce81
yahyacodes <yahya.codin@gmail.com>
2023-10-07 13:40:44 +0300

    m

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
 // document.addEventListener("DOMContentLoaded", () => {});
-const movieTitle = document.querySelector(".movie-title");
-const runTime = document.querySelector(".run-time");
-const movieCap = document.querySelector(".capacity");
-const movieShowTime = document.querySelector(".show-time");
-const tickSold = document.querySelector(".tickets-sold");
-const movieDes = document.querySelector(".movie-description");
+const moviesList = document.querySelector(".movies-list");
+
+const renderMovies = (movie) => {
+  const card = document.createElement("li");
+  card.className = "card";
+  card.innerHTML = `
+        <img src = '${movie.poster}'>
+        <h2>Title: ${movie.title}</h2>
+        <p>Description: <span>${movie.description}</span></p>
+        <h4>Runtime: ${movie.runtime} mins</h4>
+        <h4>Available Tickets: ${
+          movie.capacity - movie.tickets_sold
+        } tickets</h4>
+        <h4>Start Time: ${movie.showtime}</h4>
+        <button class='btn-ticket'>Buy Ticket</button>
+        `;
+  moviesList.appendChild(card);
+  const btnTicket = document.querySelector(".btn-ticket");
+  btnTicket.addEventListener("click", () => {
+    console.log("clicked");
+  });
+};
 
 fetch("http://localhost:3000/films")
   .then((res) => res.json())
-  .then((data) =>
-    data.forEach((movie) => {
-      movieTitle.innerHTML = movie.title;
-      runTime.innerHTML = movie.runtime;
-      movieCap.innerHTML = movie.capacity;
-      movieShowTime.innerHTML = movie.showtime;
-    })
-  );
-
-const btn = document.querySelector(".btn-class");
-btn.addEventListener("click", () => {
-  console.log("Button clicked");
-});
+  .then((data) => data.forEach((movie) => renderMovies(movie)));
