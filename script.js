a11ee1b4debde74b15f0c888c8169e53b026f691
yahyacodes <yahya.codin@gmail.com>
2023-10-07 16:44:49 +0300

    m

--- script.js
+++ script.js
@@ -9,17 +9,33 @@ const renderMovies = (movie) => {
         <h2>Title: ${movie.title}</h2>
         <p>Description: <span>${movie.description}</span></p>
         <h4>Runtime: ${movie.runtime} mins</h4>
-        <h4>Available Tickets: ${
+        <h4 id='movie-tickets'>Available Tickets: ${
           movie.capacity - movie.tickets_sold
         } tickets</h4>
         <h4>Start Time: ${movie.showtime}</h4>
-        <button class='btn-ticket'>Buy Ticket</button>
+        <button id='btn-ticket'>Buy Ticket</button>
         `;
   moviesList.appendChild(card);
-  const btnTicket = document.querySelector(".btn-ticket");
-  btnTicket.addEventListener("click", () => {
-    console.log("clicked");
+  card.querySelector("#btn-ticket").addEventListener("click", () => {
+    let buyTickets = movie.capacity - movie.tickets_sold;
+    buyTickets--;
+    card.querySelector(
+      "#movie-tickets"
+    ).textContent = `Available Tickets: ${buyTickets} tickets`;
+    updateTickets(movie);
   });
+};
+
+const updateTickets = (ticket) => {
+  fetch(`http://localhost:3000/films/${ticket.id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(ticket),
+  })
+    .then((res) => res.json())
+    .then((data) => console.log(data));
 };
 
 fetch("http://localhost:3000/films")
